c5dc9ebbb4827ad12d04e3863e2cc110f053dc6f
K1uV <61290607+K1uV@users.noreply.github.com>
2020-03-13 14:37:46 +0200

    Add files via upload

---
+++ lekziya2_task2.py
@@ -0,0 +1,5 @@
+a = "Pasha"
+b = "Pavlenko"
+d = " "
+c = a + d + b 
+print(c)
---
+++ lekziya2_task3.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2 
+
+plus = a + b
+minus = a - b 
+dileeenya = a/b 
+mnoshennya = a*b
+
+print('\n',plus,'\n',minus,'\n',dileeenya,'\n',mnoshennya)
